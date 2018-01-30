11ac6a0d64c866ffe80381436f049141da1df134
dhg <gibsonhdrew@gmail.com>
2018-01-29 19:15:33 -0500

    added visual art section and restructured site

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
 setTimeout(function() {
   $('.cb2').click(function(event) {
     event.stopPropagation();
+    $('#visualartSlider').hide()
     $('#performanceSlider').toggle()
   })
 
+  $('.cb3').click(function(event) {
+    event.stopPropagation();
+    $('#performanceSlider').hide()
+    $('#visualartSlider').toggle()
+  })
+
   $('.slideNavs').click(function() {
-    $('#performanceSlider').toggle()
+    $('#performanceSlider').hide()
+    $('#visualartSlider').hide()
   })
 
   $(document).click( function(){
     $('#performanceSlider').hide();
+    $('#visualartSlider').hide();
   });
+
 }, 100)
